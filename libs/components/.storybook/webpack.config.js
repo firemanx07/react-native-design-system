36b41b22d13381e2bb89505024a0dcccb1371488
Ghassen mellassi <ghassen.mellasi@proxym-it.com>
2023-01-29 05:31:01 +0100

    revert

--- libs/components/.storybook/webpack.config.js
+++ libs/components/.storybook/webpack.config.js
@@ -2,28 +2,22 @@ const { resolve } = require('path');
 const { withUnimodules } = require('@expo/webpack-config/addons');
 
 module.exports = ({ config }) => {
-  return withUnimodules(
-    { ...config,
-      output: {
-        publicPath: '/react-native-starter/proxym-rn-design-system/',
-      } },
-    {
-      projectRoot: resolve(__dirname, '../'),
-      babel: {
-        dangerouslyAddModulePathsToTranspile: [
-          // Ensure that all packages starting with @proxym are transpiled.
-          '@proxym',
-        ],
-      },
-      module: {
-        rules: [
-          {
-            test: /\.ttf$/,
-            loader: 'url-loader',
-            include: resolve(__dirname, '../src/assets/fonts'),
-          },
-        ],
-      },
+  return withUnimodules(config, {
+    projectRoot: resolve(__dirname, '../'),
+    babel: {
+      dangerouslyAddModulePathsToTranspile: [
+        // Ensure that all packages starting with @proxym are transpiled.
+        '@proxym',
+      ],
     },
-  );
+    module: {
+      rules: [
+        {
+          test: /\.ttf$/,
+          loader: 'url-loader',
+          include: resolve(__dirname, '../src/assets/fonts'),
+        },
+      ],
+    },
+  });
 };
