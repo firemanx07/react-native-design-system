651a5f42a027834b95ae1620808d18c900fa8165
Ghassen mellassi <ghassen.mellasi@proxym-it.com>
2023-01-29 14:27:19 +0100

    story book deploy config

--- libs/components/.storybook/webpack.config.js
+++ libs/components/.storybook/webpack.config.js
@@ -1,9 +1,13 @@
 const { resolve } = require('path');
 const { withUnimodules } = require('@expo/webpack-config/addons');
 
+const isProduction = process.env.NODE_ENV === 'production';
+const baseUrl = isProduction ? '/react-native-starter/proxym-rn-design-system/' : '/';
+
 module.exports = ({ config }) => {
   return withUnimodules(config, {
     projectRoot: resolve(__dirname, '../'),
+    publicPath: baseUrl,
     babel: {
       dangerouslyAddModulePathsToTranspile: [
         // Ensure that all packages starting with @proxym are transpiled.
