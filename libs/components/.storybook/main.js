e400cf78af855dc8c7f2b4e7937446c265f701b1
Ghassen mellassi <ghassen.mellasi@proxym-it.com>
2023-01-30 14:06:19 +0100

    story book deploy config

--- libs/components/.storybook/main.js
+++ libs/components/.storybook/main.js
@@ -1,19 +1,24 @@
+const isProduction = process.env.NODE_ENV === 'production';
+const baseUrl = isProduction ? '/react-native-starter/proxym-rn-design-system/' : '/';
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-essentials', 'storybook-addon-mock/register'],
+  features: {
+    storyStoreV7: true,
+  },
   webpackFinal: config => {
     // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
-    const fileLoaderRule = config.module.rules.find(
-      rule => rule.test && rule.test.test('.svg'),
-    );
+    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'));
     fileLoaderRule.exclude = /\.svg$/;
-
     config.module.rules.push({
       test: /\.svg$/,
       enforce: 'pre',
-      loader: require.resolve('@karankalsi/react-native-svg-loader'),
+      loader: require.resolve('@karankalsi/react-native-svg-loader')
     });
 
-    return config;
-  },
+    return {...config, output: { ...config.output, publicPath: baseUrl } };
+    },
+  docs: {
+    autodocs: true
+  }
 };
